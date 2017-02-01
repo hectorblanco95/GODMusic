484b266a799da571866d743a2c6a239ee450098a
txoba <gerardjovani96@gmail.com>
2017-02-01 18:05:36 +0100

    pussi

---
+++ iniciosesion.js
@@ -0,0 +1,28 @@
+
+$(document).ready(rellenar);
+
+function rellenar(){
+    $("#botoninicio").click(inicio);   
+    $("#botonregistro").click(registro); 
+}
+function inicio(){
+    $(this).animate({"top":"6px","width":"180px","height":"137px"},{"duration":1000});
+    $("#botonregistro").fadeOut();
+    $(this).html("<label for='inputNombre'>Nombre de usuario:</label>"+
+            "<input name='nombre' id='inputNombre' type='text'>"+
+            "<label for='inputPass'>Contraseña:</label>"+
+            "<input name='pass' id='pass' type='password'>"+
+            "<input id='submitinicio' type='submit' value='Iniciar Sesión'>");      
+}
+function registro(){
+    $(this).animate({"top":"6px","width":"180px","height":"137px"},{"duration":1000});
+    $("#botoninicio").fadeOut();
+    $(this).html("<label for='inputNombre'>Nombre de usuario:</label>"+
+            "<input name='nombre' id='inputNombre' type='text'>"+
+            "<label for='inputPass'>Contraseña:</label>"+
+            "<input name='pass' id='pass' type='password'>"+
+            "Fan<input id='fan' type='radio'/>"+
+            "Musico<input id='musico' type='radio'/>"+
+            "Local<input id='local' type='radio'/>"+
+            "<input id='submitregistro' type='submit' value='Registrarse'>"); 
+}
