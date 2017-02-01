fcafa41c3d17354fe4d5a2f89163aaed67d724fa
txoba <gerardjovani96@gmail.com>
2017-02-01 16:42:55 +0100

    nombres

--- PERFILFAN/perfilfan.php
+++ PERFILFAN/perfilfan.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>perfilfan</title>
+        <title>Perfil Fan</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="perfilfan.css" rel="stylesheet" type="text/css"/>
--- REGISTRO/registro.php
+++ REGISTRO/registro.php
@@ -1,7 +1,7 @@
 <html>
     <head>
     
-        <title>registrarse</title>
+        <title>Registro</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <link href="registro.css" rel="stylesheet" type="text/css"/>
