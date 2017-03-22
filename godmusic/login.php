638d6dddf60551e9600a3e95725863b5a7b1af4d
hectorblanco95 <hectorblancofelipe@gmail.com>
2017-03-22 03:00:42 +0000

    Commit8

--- godmusic/login.php
+++ godmusic/login.php
@@ -1,19 +1,20 @@
 <?php
 session_start();
 require_once "Functions/bbdd.php";
-    if (isset($_POST['register-submit'])==true) {
+    if (isset($_POST['register-submit'])) {
         // Recogemos las variables del POST
         $username = $_POST['username'];
         $mail = $_POST['email'];
         $pass = $_POST['password'];
         $pass2 = $_POST['confirm-password'];
         $usu = $_POST['usu'];
-    
+        if($pass!=$pass2){
+            echo 'las contraseñas no coinciden';
+        }else {
         insertarUsu($username, $pass, $mail, $usu);
         header("Location: HTML/index_home.php");
-        
-        
-    } if (isset($_POST['login-submit'])==true){
+        }
+    } if (isset($_POST['login-submit'])){
         // Recogemos las variables del POST
         $username = $_POST['username'];
         $pass = $_POST['password'];
@@ -34,7 +35,6 @@ require_once "Functions/bbdd.php";
            $_SESSION['direccion'] = $fila['direccion'];
            $_SESSION['perfil'] = $fila['perfil'];
             header("Location: HTML/profile.php");
-
         } else{
             header("Location: HTML/index_home.php");
         }
